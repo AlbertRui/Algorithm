d193d83803b14b83d7f85669ca714a1b1127ad6a
AlbertRui <isruizhang@gmail.com>
2018-03-28 17:05:12 +0800

    add Three Num

---
+++ base/src/analysis/ClassTest.java
@@ -0,0 +1,20 @@
+package com.algs4.alganaly;
+
+import analysis.ThreeSum;
+
+/**
+ * 测试
+ *
+ * @author ZhangRui
+ * 2017年10月21日,下午12:55:08
+ */
+public class ClassTest {
+
+    public static void main(String[] args) {
+        ThreeSum threeSum = new ThreeSum();
+        int[] num = threeSum.readAllInt();
+        System.out.println(threeSum.count(num));
+        System.out.println("=========================");
+        threeSum.printNum(num);
+    }
+}
