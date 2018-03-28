d49d565a0d1d4f434886be080e1c39e891254c04
AlbertRui <isruizhang@gmail.com>
2018-03-28 18:06:30 +0800

    completed the fixed capacity stack

--- base/src/algs/stack/array/FixedCapacityStack.java
+++ base/src/algs/stack/array/FixedCapacityStack.java
@@ -1,8 +1,35 @@
 package algs.stack.array;
 
 /**
+ * 基于数组实现定容栈
+ *
  * @author AlbertRui
  * @date 2018-03-28 17:59
  */
-public class FixedCapacityStack {
+@SuppressWarnings("ALL")
+public class FixedCapacityStack<Item> {
+
+    private Item[] a;
+    private int N;
+
+    public FixedCapacityStack(int cap) {
+        a = (Item[]) new Object[cap];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        a[N++] = item;
+    }
+
+    public Item pop() {
+        return a[--N];
+    }
+
 }
