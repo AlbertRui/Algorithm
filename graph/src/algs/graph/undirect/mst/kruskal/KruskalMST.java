a64ef8bed530e77621d8035eb9ebac8bfe760c98
AlbertRui <isruizhang@gmail.com>
2018-04-02 23:23:52 +0800

    completed the Kruskal minimum spanning tree

--- graph/src/algs/graph/undirect/mst/kruskal/KruskalMST.java
+++ graph/src/algs/graph/undirect/mst/kruskal/KruskalMST.java
@@ -4,6 +4,7 @@ import algs.graph.undirect.weight.Edge;
 import algs.graph.undirect.weight.EdgeWeightedGraph;
 import algs.util.MinPQ;
 import algs.util.Queue;
+import algs.util.UF;
 
 /**
  * 最小生成树的Kruskal算法
@@ -13,13 +14,37 @@ import algs.util.Queue;
  */
 public class KruskalMST {
 
+    private double weight;
     private Queue<Edge> mst;
+
     public KruskalMST(EdgeWeightedGraph graph) {
         mst = new Queue<>();
         MinPQ<Edge> pq = new MinPQ<>();
         for (Edge edge : graph.edges()) {
             pq.insert(edge);
         }
+        UF uf = new UF(graph.V());
+
+        while (!pq.isEmpty() && mst.size() < graph.V() - 1) {
+            //从pq中得到权重最小的边和它的顶点
+            Edge edge = pq.delMin();
+            int v = edge.either();
+            int w = edge.other(v);
+            if (uf.connected(v, w)) {
+                continue;//忽略失效的边
+            }
+            uf.union(v, w);//合并分量
+            mst.enqueue(edge);//将边添加到最小生成树中
+            weight += edge.getWeight();
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    public double getTotalWeight() {
+        return weight;
     }
 
 }
