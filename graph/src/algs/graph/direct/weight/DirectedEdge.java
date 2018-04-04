61381b6ec160c2eeac1d30dc2bc733bedb5e8562
AlbertRui <isruizhang@gmail.com>
2018-04-04 15:40:24 +0800

    completed the directed edge

--- graph/src/algs/graph/direct/weight/DirectedEdge.java
+++ graph/src/algs/graph/direct/weight/DirectedEdge.java
@@ -7,4 +7,52 @@ package algs.graph.direct.weight;
  * @date 2018-04-03 21:47
  */
 public class DirectedEdge {
+
+    private final int v;//变的起点
+    private final int w;//边的终点
+    private final double weight;//边的权重
+
+    public DirectedEdge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    /**
+     * 边的权重
+     *
+     * @return
+     */
+    public double getWeight() {
+        return weight;
+    }
+
+    /**
+     * 指出这条边的起点
+     *
+     * @return
+     */
+    public int from() {
+        return v;
+    }
+
+    /**
+     * 这条边指向的顶点
+     *
+     * @return
+     */
+    public int to() {
+        return w;
+    }
+
+    /**
+     * 对象的字符串表示
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return String.format("%d->%d %.2f", v, w, weight);
+    }
+
 }
