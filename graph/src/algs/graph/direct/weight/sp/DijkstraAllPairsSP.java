a95bfc157c7d3bbb536cafc692683acf1cb24290
AlbertRui <isruizhang@gmail.com>
2018-04-04 17:42:52 +0800

    add Dijkstra all pairs shortest path

--- graph/src/algs/graph/direct/weight/sp/DijkstraAllPairsSP.java
+++ graph/src/algs/graph/direct/weight/sp/DijkstraAllPairsSP.java
@@ -2,7 +2,7 @@ package algs.graph.direct.weight.sp;
 
 /**
  * @author AlbertRui
- * @date 2018-04-04 17:30
+ * @date 2018-04-04 17:41
  */
-public class Main {
+public class DijkstraAllPairsSP {
 }
