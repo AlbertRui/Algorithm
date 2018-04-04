bbdec577b2bb97b2bc57f9021eacfb69232fdade
AlbertRui <isruizhang@gmail.com>
2018-04-04 17:49:15 +0800

    completed the Dijkstra all pairs shortest paht

--- graph/src/algs/graph/direct/weight/sp/DijkstraAllPairsSP.java
+++ graph/src/algs/graph/direct/weight/sp/DijkstraAllPairsSP.java
@@ -1,8 +1,31 @@
 package algs.graph.direct.weight.sp;
 
+import algs.graph.direct.weight.DirectedEdge;
+import algs.graph.direct.weight.EdgeWeightedDigraph;
+
 /**
+ * 任意两点之间的最短路径
+ *
  * @author AlbertRui
  * @date 2018-04-04 17:41
  */
 public class DijkstraAllPairsSP {
+
+    private DijkstraSP[] all;
+
+    DijkstraAllPairsSP(EdgeWeightedDigraph digraph) {
+        all = new DijkstraSP[digraph.V()];
+        for (int v = 0; v < digraph.V(); v++) {
+            all[v] = new DijkstraSP(digraph, v);
+        }
+    }
+
+    Iterable<DirectedEdge> path(int s, int t) {
+        return all[s].pathTo(t);
+    }
+
+    double dist(int s, int t) {
+        return all[s].distTo(t);
+    }
+
 }
